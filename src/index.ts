619415be61c6b7bcd29fc07a35d54233c1227944
Rerurate514 <rerurateyuto@gmail.com>
2025-05-24 01:42:12 +0900

    setコマンドをexport

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { TransitusServer } from "./transitus/logic/server";
 export { Router } from './transitus/logic/router';
 
 export { AppRouter } from "./transitus/components/appRouter";
+
+export { setHost } from "./transitus/logic/setConfig";
+export { setPort } from "./transitus/logic/setConfig";
