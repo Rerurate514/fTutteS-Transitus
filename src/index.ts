6c88f5a8e474e6e48a5650023e2e14a9ef96c430
Rerurate514 <rerurateyuto@gmail.com>
2025-06-25 21:56:12 +0900

    indexの更新

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export { Router } from './transitus/logic/router';
 export { AppRouter } from "./transitus/components/appRouter";
 
 export { setHost, setPort } from "./transitus/logic/setConfig";
+
+export { MiddleWare } from "./transitus/interface/middleware";
+export { DefaultRequestHandler } from "./transitus/logic/middlewares/defaultRequestHandler";
