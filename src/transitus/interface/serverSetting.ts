1878767cff4ddfc6f90d51dddd3fe2f67b65b841
Rerurate514 <rerurateyuto@gmail.com>
2025-07-02 16:22:28 +0900

    serverSettingインターフェースを作成

---
+++ src/transitus/interface/serverSetting.ts
@@ -0,0 +1,14 @@
+import { TransitusServer } from '@transitus/logic/server';
+import { Server } from 'http';
+
+export interface ITransitusServer {
+    restart(): void;
+    stop(): void;
+    getServer(): Server | undefined;
+}
+
+export interface ServerSetting { 
+    initialize(server: Server, tserver: TransitusServer): void;
+    cleanup?(): void;
+    name?: string;
+}
